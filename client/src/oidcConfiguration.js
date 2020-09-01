291ca6170b4d0419dba4f1ee778c7874bdc364ce
ludojmj <ludovic.villejarno@gmail.com>
2020-09-01 17:24:12 +0200

    Build client and server automation

--- client/src/oidcConfiguration.js
+++ client/src/oidcConfiguration.js
@@ -15,20 +15,6 @@ const configuration = {
       }
     },
     {
-      origin: 'http://localhost:5000',
-      config: {
-        client_id: 'interactive.public.short',
-        redirect_uri: 'http://localhost:5000/authentication/callback',
-        response_type: 'code',
-        post_logout_redirect_uri: 'http://localhost:5000/',
-        scope: 'openid profile email api offline_access',
-        authority: 'https://demo.identityserver.io',
-        silent_redirect_uri: 'http://localhost:5000/authentication/silent_callback',
-        automaticSilentRenew: true,
-        loadUserInfo: true
-      }
-    },
-    {
       origin: 'https://localhost:5001',
       config: {
         client_id: 'interactive.public.short',
@@ -38,6 +24,34 @@ const configuration = {
         scope: 'openid profile email api offline_access',
         authority: 'https://demo.identityserver.io',
         silent_redirect_uri: 'https://localhost:5001/authentication/silent_callback',
+        automaticSilentRenew: true,
+        loadUserInfo: true
+      }
+    },
+    {
+      origin: 'https://rita.azurewebsites.net',
+      config: {
+        client_id: 'interactive.public.short',
+        redirect_uri: 'https://rita.azurewebsites.net/authentication/callback',
+        response_type: 'code',
+        post_logout_redirect_uri: 'https://rita.azurewebsites.net/',
+        scope: 'openid profile email api offline_access',
+        authority: 'https://demo.identityserver.io',
+        silent_redirect_uri: 'https://rita.azurewebsites.net/authentication/silent_callback',
+        automaticSilentRenew: true,
+        loadUserInfo: true
+      }
+    },
+    {
+      origin: 'https://ludal.azurewebsites.net',
+      config: {
+        client_id: 'interactive.public.short',
+        redirect_uri: 'https://ludal.azurewebsites.net/authentication/callback',
+        response_type: 'code',
+        post_logout_redirect_uri: 'https://ludal.azurewebsites.net/',
+        scope: 'openid profile email api offline_access',
+        authority: 'https://demo.identityserver.io',
+        silent_redirect_uri: 'https://ludal.azurewebsites.net/authentication/silent_callback',
         automaticSilentRenew: true,
         loadUserInfo: true
       }
